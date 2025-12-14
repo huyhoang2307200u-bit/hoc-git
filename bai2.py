0d23b9f236446e79ba53d6759ef42076629a4f3b
huyhoang2307200u-bit <huyhoang2307200u@gmail.com>
2025-12-14 13:38:22 +0700

    baitap

--- bai2.py
+++ bai2.py
@@ -1,2 +1,17 @@
-i = 10
-print(i)
+import math
+def phantichso(n):
+    ds = []
+    for i in range(2, int(math.sqrt(n)) + 1):
+        while n % i == 0:
+            ds.append(i)
+            n //= i
+    if n > 1:
+        ds.append(n)
+    return ds
+
+
+
+if __name__ == "__main__":
+
+    n = int(input("Nhap so can phan tich: "))
+    print(phantichso(n))
